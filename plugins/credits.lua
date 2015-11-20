36e7fa69acbd5e4db47769711064bd653e9bdce3
ArmanIr <memfi4@yahoo.com>
2015-11-20 15:41:08 +0330

    Create credits.lua

---
+++ plugins/credits.lua
@@ -0,0 +1,19 @@
+do
+
+function run(msg, matches)
+  return "ThisBot CREATED'DEVELOPED'MANAGED BY @ThisIsArman 😘
+end
+
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/Credits$",
+    "^!Credits$",
+    "^/credtis$",
+    "^!credits$",
+  },
+  run = run
+}
+
+end
