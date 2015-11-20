825bbd68fbe6b0ee67ce95942c065936610353ac
ArmanIr <memfi4@yahoo.com>
2015-11-20 15:38:30 +0330

    Create starter.lua

---
+++ plugins/starter.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return "Hi😝\nYou Can Create Group With↙️\n!creategroup groupname\nYou Can Also Join Our News Channel(GoodForYou)↙️\nhttps://telegram.me/joinchat/BhviQzur-GTHEOlVngENbw\nCreated'Developed and Managed byhttps://telegram.me/joinchat/BhviQzur-GTHEOlVngENbw\n@ThisIsArman
+end
+
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/start$",
+    "^!Start$",
+    "^/Start$",
+    "^!start$",
+   "^!help$",
+   "^/help$",
+   "^!Help$",
+  },
+  run = run
+}
+
+end
